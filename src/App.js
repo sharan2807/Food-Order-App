ac2b835ddb13122de9de796bc96031110629a489
sharan2807 <sharancold98@gmail.com>
2024-01-31 14:09:27 +0530

    Adding the Meals Summary component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import { Fragment } from "react";
 import Header from "./components/Layout/Header";
+import MealsSummary from "./components/Meals/MealsSummary";
 
 function App() {
   return (
     <Fragment>
       <Header />
+      <main>
+        <MealsSummary />
+      </main>
     </Fragment>
   );
 }
